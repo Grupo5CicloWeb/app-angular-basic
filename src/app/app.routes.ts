615f6b221e82f4345f1fd18fa3f70ba2ec2813e7
Grupo5CicloWeb <cicloc504@gmail.com>
2024-06-01 14:20:56 -0500

    ejercicios básicos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FundamentosComponent } from './fundamentos/fundamentos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "fundamentos", component: FundamentosComponent}
+];
